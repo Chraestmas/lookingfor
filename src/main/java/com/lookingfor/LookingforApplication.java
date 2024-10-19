6758edbe2442eb3dda3d3e262767c21344206cb6
Chraestmas <jaewoo75.chun@gmail.com>
2024-10-19 16:41:57 +0900

    first commit

---
+++ src/main/java/com/lookingfor/LookingforApplication.java
@@ -0,0 +1,13 @@
+package com.lookingfor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookingforApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookingforApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lookingfor/LookingforApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lookingfor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookingforApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
