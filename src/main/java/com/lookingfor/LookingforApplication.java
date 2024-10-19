8fd39469d10e5ef13a50f5772d018f65b6ba6f6f
2408-web <it.korea2024@gmail.com>
2024-10-19 17:17:19 +0900

    배상엽 수정

--- src/main/java/com/lookingfor/LookingforApplication.java
+++ src/main/java/com/lookingfor/LookingforApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LookingforApplication {
 
 	public static void main(String[] args) {
+		//코드 수정함!
 		SpringApplication.run(LookingforApplication.class, args);
 	}
 
