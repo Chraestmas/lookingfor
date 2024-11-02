a5db11505540405174d0d2a1db27fb34af98fb22
Chraestmas <jaewoo75.chun@gmail.com>
2024-11-02 18:31:08 +0900

    pull

--- src/main/java/com/lookingfor/controller/ItemController.java
+++ src/main/java/com/lookingfor/controller/ItemController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lookingfor.dto.ItemDTO;
@@ -28,7 +26,7 @@ public class ItemController {
 		this.is = is;
 	}
 	
-	// id로 모임을 조회하는 api
+	// id로 item을 조회하는 api
 	@GetMapping("/api/item/{id}")
 	public ResponseEntity<ItemDTO> getItemById(@PathVariable("id") Integer id) {
 		return ResponseEntity.status(200).body(is.getItemById(id));
@@ -44,13 +42,6 @@ public class ItemController {
 		
 		
 		return ResponseEntity.status(200).body(is.getItems(page, size, foundYn, categoryId, itemName));
-	}
-	
-	@PostMapping("/api/item")
-	public ResponseEntity<ItemDTO> createNewItem(@RequestBody ItemDTO itemDto) {
-//		System.out.println(itemDto);
-
-		return ResponseEntity.status(200).body(is.createItem(itemDto));
 	}
 }
 
