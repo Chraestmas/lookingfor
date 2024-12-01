c7f94b359403cbf0cd3e87130602fc2647ac78e9
Chraestmas <jaewoo75.chun@gmail.com>
2024-12-01 10:23:50 +0900

    resolved conflict

--- src/main/java/com/lookingfor/repository/UserRepository.java
+++ src/main/java/com/lookingfor/repository/UserRepository.java
@@ -4,6 +4,6 @@ package com.lookingfor.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.lookingfor.entity.UserEntity;
 
-public interface UserRepository extends JpaRepository  <UserEntity, String> {
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 
 }
